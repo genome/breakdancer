0adc1fc16e7e468f3d438b79286bce5fd367a8cf
xfan <xfan@127c0c0e-971a-0410-ad48-868c1a942d8d>
2010-05-11 23:09:05 +0000

    051110 using samtools. Data structure still needs to be changed.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/src/BreakDancer@58589 127c0c0e-971a-0410-ad48-868c1a942d8d

--- AlnParser.h
+++ AlnParser.h
@@ -1,7 +1,9 @@
 #ifndef ALNPARSER_H
 #define ALNPARSER_H
+#include "sam.h"
+#include "bam.h"
 
-struct t_buf {
+/*struct t_buf {
 	string readname;
 	string chr;
 	int pos;
@@ -14,7 +16,22 @@ struct t_buf {
 	int flag;
 };
 
+// no need for the struct for a map(hash)
 struct readgroup_platform_buf {
 	int readgroup;
 	string platform;	
-};
+};*/
+
+typedef struct {
+	bam1_core_t core;
+	int l_aux, data_len, m_data;
+	uint8_t *data;
+//	int readlen; -> bam2_t.core.l_qseq
+	char ori;
+//	int flag; -> bam2_t.core.flag
+	char *readgroup;
+//	int qual; -> bam2_t.core.qual
+//	int pos; -> bam2_t.core.pos
+} bam2_t;
+
+#define bam_init2() ((bam2_t*)calloc(1, sizeof(bam2_t)))
