603216cbe52329e35986c9f660d7bbde68e44980
xfan <xfan@127c0c0e-971a-0410-ad48-868c1a942d8d>
2010-05-12 19:00:44 +0000

    Compilable, 051210
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/src/BreakDancer@58628 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ BreakDancerMax.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <stdio.h>
+#include <cstdlib>
+#include <list>
+#include <iomanip>
+#include <cmath>
+#include <math.h>
+#include <time.h>
+#include <map>
+#include "sam.h"
+#include "bam.h"
+
+//extern std::map<char *, std::string> readgroup_platform;
