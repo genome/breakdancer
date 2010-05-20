5fdec8096990d857f92e334d39699054b2ed54ef
xfan <xfan@127c0c0e-971a-0410-ad48-868c1a942d8d>
2010-05-20 23:26:32 +0000

    052010 Compiling.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/src/BreakDancer@59007 127c0c0e-971a-0410-ad48-868c1a942d8d

--- BreakDancerMax.h
+++ BreakDancerMax.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <stdio.h>
+#include <stdlib.h>
 #include <cstdlib>
 #include <list>
 #include <iomanip>
@@ -10,46 +11,51 @@
 #include <math.h>
 #include <time.h>
 #include <map>
+#include <assert.h>
+#include <sstream>
 #include "sam.h"
 #include "bam.h"
 #include "ksort.h"
 #include "AlnParser.h"
 #include "Poisson.h"
+#include "samtools.h"
 
 using namespace std;
 
-typedef struct {
-     uint64_t u, v;
-}pair64_t;
+#define LZERO -99;
 
-typedef struct {
-	int i;
-	uint64_t pos, idx;
-	bam1_t *b;
-} heap1_t;
+#define ZERO exp(LZERO);
 
-void Analysis (string lib, bam1_t *b, vector<vector<string> > &reg_seq, vector<int,vector<int> > &reg_name, map<string,vector<int> > &read, map<int, vector<vector<string> > > &regs, int &begins, int &beginc, int &lasts, int &lastc, int &idx_buff, int buffer_size, int &nnormal_reads, int min_len, int &normal_switch, int &reg_idx, int transchr_rearrange, int min_map_qual, int Illumina_long_insert, int prefix_fastq, map<uint32_t, map<string,int> > &x_readcounts, int reference_len, int fisher);
+/*template <class T>
+T from_string(const std::string& s, 
+                 std::ios_base& (*f)(std::ios_base&))*/
 
-void buildConnection(map<string,vector<int> > &read, map<int,vector<int> > &reg_name, map<int,vector<vector<string> > > &regs);
+void Analysis (string lib, bam1_t *b, vector<vector<string> > &reg_seq, vector<int,vector<int> > &reg_name, map<string,vector<int> > &read, map<int, vector<vector<string> > > &regs, int *begins, int *beginc, int *lasts, int *lastc, int *idx_buff, int buffer_size, int *nnormal_reads, int min_len, int *normal_switch, int *reg_idx, int transchr_rearrange, int min_map_qual, int Illumina_long_insert, int prefix_fastq, map<uint32_t, map<string,int> > &x_readcounts, int reference_len, int fisher, map<string,string> ReadsOut);
 
-int PutativeRegion(vector<int> rnode, map<int,vector<int> > &reg_name);
+void buildConnection(map<string,vector<int> > &read, map<int,vector<int> > &reg_name, map<int,vector<vector<string> > > &regs, int min_read_pair, string dump_BED, int max_readlen, map<string,string> SVtype, string prefix_fastq, map<string,ofstream> ReadsOut, map<uint32_t,string> SVtype);
 
-float ComputeProbScore(vector<int> rnode, map<string,int> rlibrary_readcount, uint32_t type, map<uint32_t, map<string,int> > &x_readcounts, int reference_len, int fisher);
+float ComputeProbScore(vector<int> rnode, map<string,int> rlibrary_readcount, uint32_t type, map<uint32_t, map<string,int> > &x_readcounts, int reference_len, int fisher, map<int, vector<int> > &reg_name);
 
-void EstimatePriorParameters(map<string,string> &fmaps, map<string,string> &readgroup_library, map<string, float> &mean_insertsize, map<string, float> &std_insertsize, map<string,float> &uppercutoff, map<string,float> &lowercutoff, map<string,float> &readlens, int chr);
+void EstimatePriorParameters(map<string,string> &fmaps, map<string,string> &readgroup_library, map<string, float> &mean_insertsize, map<string, float> &std_insertsize, map<string,float> &uppercutoff, map<string,float> &lowercutoff, map<string,float> &readlens, int chr, int cut_sd, int min_map_qual, map<string, string> readgroup_platform);
 
 float mean(vector<int> &stat);
 
 float standard_deviation(vector<int> &stat, float mean);
 
-int PutativeRegion(vector<int> rnode, map<int,vector<int> > &reg_name);
+int PutativeRegion(vector<int> &rnode, map<int,vector<int> > &reg_name);
 
-bamFile ReadBamChr_prep(string chr_str, string bam_name, int &tid, int &beg, int &end, samfile_t *in, pair64_t *off, int &n_off);
+bamFile ReadBamChr_prep(string chr_str, string bam_name, int *tid, int *beg, int *end, samfile_t *in, pair64_t *off, int *n_off);
 
-int ReadBamChr(bam1_t *b, bamFile fp, int tid, int beg, int end, uint64_t &curr_off, int &i, int &n_seeks, pair64_t *off, int n_off);
+int ReadBamChr(bam1_t *b, bamFile fp, int tid, int beg, int end, uint64_t *curr_off, int *i, int *n_seeks, pair64_t *off, int n_off);
 
 int MergeBams_prep(string *fn, int n, bamFile *fp, heap1_t *heap);
 
 int MergeBamsChr_prep(string *fn, int n, bamFile *fp, heap1_t *heap, string chr_str, int *tid, int *beg, int *end, samfile_t **in, pair64_t **off, int *n_off);
 
 string get_from_line(string line,string search,int flag);
+
+string get_from_line_two(string line,string search1,string search2,int flag2);
+
+string itoa(int i);
+
+int atoi(string str);
