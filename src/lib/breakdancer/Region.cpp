92a728f352a8c919238326a0c37c786b574e2861
Dave Larson <dlarson@genome.wustl.edu>
2013-04-16 17:22:29 -0500

    minor refactor and a bugfix due to uninitialized param

--- src/lib/breakdancer/Region.cpp
+++ src/lib/breakdancer/Region.cpp
@@ -6,6 +6,7 @@ namespace region {
         : begins(chrom_id)
           , beginc(start)
           , lastc(end)
+          , num_normal_reads(num_normal_reads)
     {
 
         //Copy over the reads.
@@ -18,12 +19,13 @@ namespace region {
     void Region::set_read_counts(map<string, uint32_t> const& num_ROI_reads, map<string, uint32_t> const& num_FR_reads) {
         read_count_ROI_map = map<string, uint32_t>(num_ROI_reads);
         for(map<string, uint32_t>::const_iterator num_FR_reads_it = num_FR_reads.begin(); num_FR_reads_it != num_FR_reads.end(); ++ num_FR_reads_it) {
-            string lib = (*num_FR_reads_it).first;
-            if(read_count_ROI_map.find(lib) == read_count_ROI_map.end())
+            string const& lib = (*num_FR_reads_it).first;
+            map<string, uint32_t>::iterator roi_it = read_count_ROI_map.find(lib);
+            if(roi_it == read_count_ROI_map.end())
                 read_count_FR_map[lib] = (*num_FR_reads_it).second;
             else{
-                assert(read_count_ROI_map[lib] > (*num_FR_reads_it).second); 
-                read_count_FR_map[lib] =  (*num_FR_reads_it).second - read_count_ROI_map[lib];
+                assert((*roi_it).second <= (*num_FR_reads_it).second); 
+                read_count_FR_map[lib] =  (*num_FR_reads_it).second - (*roi_it).second;
             }
         }
 
