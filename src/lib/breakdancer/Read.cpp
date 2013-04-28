fefdb51a075017092a2e85f22f7623ee6ea34d77
Dave Larson <dlarson@genome.wustl.edu>
2013-04-27 23:05:51 -0500

    Multiple changes: * Reorganized code a bit so methods and variables are better ordered * Now uses initializer lists more extensively * Assignment operator now frees old memory, even if assigner has a NULL   record

--- src/lib/breakdancer/Read.cpp
+++ src/lib/breakdancer/Read.cpp
@@ -5,16 +5,11 @@
 using namespace std;
 using namespace breakdancer;
 
-Read::Read(bam1_t const* record, string const& format, map<string, string> const& readgroup_platform, map<string, string> const& readgroup_library) {
+Read::Read(bam1_t const* record, string const& format, map<string, string> const& readgroup_platform, map<string, string> const& readgroup_library) :
+    _record(bam_init1()), _query_name_cached(false), _query_seq_cached(false), _quality_string_cached(false) {
     //create copy of bam record
-    _record = bam_init1(); 
     bam_copy1(_record, record);
     
-    //set caching operators (could be an initializer list)
-    _query_name_cached = false;
-    _query_seq_cached = false;
-    _quality_string_cached = false;
-
     //set flag and qualities
     _bdflag = _determine_bdflag();
     bdqual = _determine_bdqual();
@@ -35,7 +30,7 @@ Read::Read(bam1_t const* record, string const& format, map<string, string> const
     _string_record.push_back(library);
 }
 
-Read::Read(const Read& other) {
+Read::Read(const Read& other) : _query_name(other._query_name), _query_name_cached(other._query_name_cached), _query_sequence(other._query_sequence), _query_seq_cached(other._query_seq_cached), _quality_string(other._quality_string), _quality_string_cached(other._quality_string_cached), _bdflag(other._bdflag), _string_record(other._string_record), bdqual(other.bdqual), readgroup(other.readgroup), platform(other.platform), library(other.library) {
     if(other._record) {
         _record = bam_init1();
         bam_copy1(_record, other._record);
@@ -43,18 +38,6 @@ Read::Read(const Read& other) {
     else {
         _record = NULL;
     }
-    _query_name = other._query_name;
-    _query_name_cached = other._query_name_cached;
-    _query_sequence = other._query_sequence;
-    _query_seq_cached = other._query_seq_cached;
-    _quality_string = other._quality_string;
-    _quality_string_cached = other._quality_string_cached;
-    _bdflag = other._bdflag;
-    bdqual = other.bdqual;
-    platform = other.platform;
-    library = other.library;
-    readgroup = other.readgroup;
-    _string_record = other._string_record;
 }
 
 Read::~Read() {
@@ -72,11 +55,15 @@ string Read::operator[](std::vector<std::string>::size_type idx) const {
 
 Read& Read::operator=(const Read& other) {
     if(this != &other) {
-        bam1_t* old_record = NULL;
+        if(_record) {
+            bam_destroy1(_record);
+        }
         if(other._record != NULL) {
-            old_record = _record;
             _record = bam_init1();
             bam_copy1(_record, other._record);
+        }
+        else {
+            _record = NULL;
         }
         _bdflag = other._bdflag;
         bdqual = other.bdqual;
@@ -90,9 +77,6 @@ Read& Read::operator=(const Read& other) {
         _quality_string = other._quality_string;
         _quality_string_cached = other._quality_string_cached;
         _string_record = other._string_record;
-        if(old_record) {
-            bam_destroy1(old_record);
-        }
     }
     return *this;
 }
@@ -106,54 +90,15 @@ string Read::_readgroup() {
     }
 }
 
-string const& Read::query_name() {
-    if(!_query_name_cached) {
-        _query_name = string(bam1_qname(_record));
-        _query_name_cached = true;
-    }
-    return _query_name;
-}
-
-string const& Read::query_sequence() {
-    if(!_query_seq_cached) {
-        uint8_t* seq_ptr = bam1_seq(_record);
-        if(_record->core.l_qseq) {
-            string seq;
-            seq.reserve(_record->core.l_qseq);
-            for(int i = 0; i < _record->core.l_qseq; ++i) {
-                seq += bam_nt16_rev_table[bam1_seqi(seq_ptr, i)];
-            }
-            _query_sequence = seq;
-        }
-        else {
-            _query_sequence = "*"; //or maybe throw? I dunno
-        }
-        _query_seq_cached = true;
-    }
-    return _query_sequence;
-}
-
-string const& Read::quality_string() {
-    if(!_quality_string_cached) {
-    uint8_t* qual_ptr = bam1_qual(_record);
-    if(*qual_ptr != 0xff) { //fi 0xff then there is no qual string in BAM
-        string qual;
-        qual.reserve(_record->core.l_qseq);
-        for(int i = 0; i < _record->core.l_qseq; ++i) {
-            qual += char(qual_ptr[i] + 33);
-        }
-        _quality_string = qual;
+string Read::_library(map<string, string> const& readgroup_library) {
+    //contrary to the regular code, let's assume we've fixed things so that the map returns the filename if the readgroup is empty.
+    map<string, string>::const_iterator lib = readgroup_library.find(readgroup);
+    if(lib != readgroup_library.end()) {
+        return lib->second;
     }
     else {
-        _quality_string = "*";
+        return "";
     }
-        _quality_string_cached = true;
-    }
-    return _quality_string;
-}
-
-string Read::ori() {
-    return  _record->core.flag & 0x0010 ? "-" : "+";
 }
 
 string Read::_platform(map<string, string> const& readgroup_platform) {
@@ -163,17 +108,6 @@ string Read::_platform(map<string, string> const& readgroup_platform) {
     }
     else {
         return "illumina";
-    }
-}
-
-string Read::_library(map<string, string> const& readgroup_library) {
-    //contrary to the regular code, let's assume we've fixed things so that the map returns the filename if the readgroup is empty.
-    map<string, string>::const_iterator lib = readgroup_library.find(readgroup);
-    if(lib != readgroup_library.end()) {
-        return lib->second;
-    }
-    else {
-        return "";
     }
 }
 
@@ -191,6 +125,7 @@ int Read::_determine_bdqual() {
 //FIXME This is practically illegible and there are many flag definitions that need to be added
 //In addition, there is some caching that could happen to make this whole thing shorter
 //and less computationally expensive
+//ideally the majority of this code could be pulled into another class.
 pair_orientation_flag Read::_determine_bdflag() {
     pair_orientation_flag flag = NA;
     int read_reversed = _record->core.flag & BAM_FREVERSE;
@@ -265,6 +200,52 @@ vector<string>::size_type Read::size() {
     return _string_record.size();
 }
 
+string const& Read::query_name() {
+    if(!_query_name_cached) {
+        _query_name = string(bam1_qname(_record));
+        _query_name_cached = true;
+    }
+    return _query_name;
+}
+
+string const& Read::query_sequence() {
+    if(!_query_seq_cached) {
+        uint8_t* seq_ptr = bam1_seq(_record);
+        if(_record->core.l_qseq) {
+            string seq;
+            seq.reserve(_record->core.l_qseq);
+            for(int i = 0; i < _record->core.l_qseq; ++i) {
+                seq += bam_nt16_rev_table[bam1_seqi(seq_ptr, i)];
+            }
+            _query_sequence = seq;
+        }
+        else {
+            _query_sequence = "*"; //or maybe throw? I dunno
+        }
+        _query_seq_cached = true;
+    }
+    return _query_sequence;
+}
+
+string const& Read::quality_string() {
+    if(!_quality_string_cached) {
+    uint8_t* qual_ptr = bam1_qual(_record);
+    if(*qual_ptr != 0xff) { //fi 0xff then there is no qual string in BAM
+        string qual;
+        qual.reserve(_record->core.l_qseq);
+        for(int i = 0; i < _record->core.l_qseq; ++i) {
+            qual += char(qual_ptr[i] + 33);
+        }
+        _quality_string = qual;
+    }
+    else {
+        _quality_string = "*";
+    }
+        _quality_string_cached = true;
+    }
+    return _quality_string;
+}
+
 void Read::set_bdflag(pair_orientation_flag const& new_flag) {
     _bdflag = new_flag;
     _string_record[5] = boost::lexical_cast<string>(new_flag); //this may need a check
@@ -272,4 +253,8 @@ void Read::set_bdflag(pair_orientation_flag const& new_flag) {
 
 pair_orientation_flag const& Read::bdflag() {
     return _bdflag;
+}
+
+string Read::ori() {
+    return  _record->core.flag & 0x0010 ? "-" : "+";
 }
--- src/lib/breakdancer/Read.hpp
+++ src/lib/breakdancer/Read.hpp
@@ -10,7 +10,20 @@ extern "C" {
 }
 
 namespace breakdancer {
-    enum pair_orientation_flag {NA=0, ARP_FF=1, ARP_FR_big_insert=2, ARP_FR_small_insert=3, ARP_RF=4, ARP_RR=8, NORMAL_FR=18, NORMAL_RF=20, ARP_CTX=32, UNMAPPED=192, MATE_UNMAPPED=64 }; //NA means not applicable.
+
+    enum pair_orientation_flag {
+        NA = 0, //NA means not applicable. 
+        ARP_FF = 1, 
+        ARP_FR_big_insert = 2, 
+        ARP_FR_small_insert = 3, 
+        ARP_RF = 4, 
+        ARP_RR = 8, 
+        NORMAL_FR = 18, 
+        NORMAL_RF = 20, 
+        ARP_CTX = 32, 
+        UNMAPPED = 192, 
+        MATE_UNMAPPED = 64 
+    };
 
     class Read {
         private:
@@ -27,6 +40,7 @@ namespace breakdancer {
             pair_orientation_flag _bdflag;
 
             std::vector<std::string> _string_record;
+
             std::string _library(std::map<std::string, std::string> const& readgroup_library);
             std::string _platform(std::map<std::string, std::string> const& readgroup_platform);
             std::string _readgroup();
@@ -43,17 +57,18 @@ namespace breakdancer {
             Read() : _record(NULL), _bdflag(NA), bdqual(0) {};
             Read(const Read& other);
             ~Read();
+
             //not really sure where the optimaly location for this const is or even what it's modifying.
             //Need to talk to Travis :(
             std::string operator[](std::vector<std::string>::size_type idx) const;
             Read& operator=(const Read& other);
 
-            void set_bdflag(pair_orientation_flag const& new_flag);
             std::vector<std::string>::size_type size();
             std::string const& query_name();
             std::string const& query_sequence();
             std::string const& quality_string();
             pair_orientation_flag const& bdflag();
+            void set_bdflag(pair_orientation_flag const& new_flag);
             std::string ori();
 
             /* Other things we will need in our interface
