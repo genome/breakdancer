c4b3ce24290dd7b9f4efe51b4bdd913448762a15
Travis Abbott <tabbott@genome.wustl.edu>
2013-07-24 20:13:03 -0500

    missing files from last commit!

---
+++ src/lib/breakdancer/ConfigMap.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <boost/container/flat_map.hpp>
+#include <boost/serialization/split_free.hpp>
+#include <boost/serialization/map.hpp>
+#include <boost/serialization/collections_save_imp.hpp>
+#include <boost/serialization/collections_load_imp.hpp>
+
+template<typename K, typename V>
+struct ConfigMap {
+    typedef boost::container::flat_map<K, V> type;
+};
+
+namespace boost {
+namespace serialization {
+
+template<class Archive, class Key, class Value, class Compare, class Allocator>
+inline void save(
+        Archive& ar,
+        boost::container::flat_map<Key, Value, Compare, Allocator> const& m,
+        const unsigned int /* version */)
+{
+    boost::serialization::stl::save_collection(ar, m);
+}
+
+template<class Archive, class Key, class Value, class Compare, class Allocator>
+inline void load(
+        Archive& ar,
+        boost::container::flat_map<Key, Value, Compare, Allocator>& m,
+        const unsigned int /* version */)
+{
+    typedef boost::container::flat_map<Key, Value, Compare, Allocator> MapType;
+
+    boost::serialization::stl::load_collection<
+        Archive,
+        MapType,
+        boost::serialization::stl::archive_input_map<Archive, MapType>,
+        boost::serialization::stl::no_reserve_imp<MapType>
+        >
+    (ar, m);
+
+}
+
+
+template<class Archive, class Key, class Value, class Compare, class Allocator>
+inline void serialize(
+        Archive& ar,
+        boost::container::flat_map<Key, Value, Compare, Allocator>& m,
+        const unsigned int version
+        )
+{
+    boost::serialization::split_free(ar, m, version);
+}
+
+}
+}
---
+++ test/lib/breakdancer/TestConfigMap.cpp
@@ -0,0 +1,69 @@
+#include "breakdancer/ConfigMap.hpp"
+
+#include <boost/archive/binary_iarchive.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/xml_iarchive.hpp>
+#include <boost/archive/xml_oarchive.hpp>
+
+#include <boost/serialization/string.hpp>
+#include <boost/serialization/nvp.hpp>
+
+#include <gtest/gtest.h>
+
+#include <sstream>
+#include <string>
+
+namespace ba = boost::archive;
+namespace bs = boost::serialization;
+
+namespace {
+    template<typename ArchOut, typename ArchIn>
+    struct Librarian {
+        typedef ArchOut output_type;
+        typedef ArchIn input_type;
+    };
+
+    typedef ConfigMap<std::string, int>::type MapType;
+}
+
+template<typename LibrarianType>
+class TestConfigMap : public ::testing::Test {
+public:
+
+    void SetUp() {
+        _m["one"] = 1;
+        _m["two"] = 2;
+        _m["three"] = 3;
+    }
+
+    // Archivers get mad if they get the map as a non-const ref
+    MapType const& testMap() const {
+        return _m;
+    }
+
+private:
+    MapType _m;
+};
+
+typedef ::testing::Types<
+    Librarian<ba::xml_oarchive, ba::xml_iarchive>,
+    Librarian<ba::text_oarchive, ba::text_iarchive>,
+    Librarian<ba::binary_oarchive, ba::binary_iarchive>
+    > MyTypes;
+
+TYPED_TEST_CASE(TestConfigMap, MyTypes);
+
+TYPED_TEST(TestConfigMap, serialize) {
+    std::stringstream ss;
+
+    typename TypeParam::output_type out(ss);
+    out << bs::make_nvp("map", this->testMap());
+
+    MapType mCopy;
+    typename TypeParam::input_type in(ss);
+    in >> bs::make_nvp("map", mCopy);
+
+    EXPECT_EQ(this->testMap(), mCopy);
+}
